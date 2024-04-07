c0cc8ca1f2a4105e88664e95c4ca1b36c5979cc4
Claudia Vidal <claudiavidalj.2010@gmail.com>
2024-04-07 19:02:27 -0400

    Conectando a la base de datos

--- Wiki/models.py
+++ Wiki/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombreUsuario = models.CharField(max_length=100, unique=True, primary_key=True)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
